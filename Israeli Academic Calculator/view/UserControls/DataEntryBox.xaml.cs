22007c635a618d4c4dcbd5bed71eda1b39b119bf
dan khilkevich <danchuk213@gmail.com>
2024-09-09 18:06:11 +0300

    made a custom controller to enter data about a course

--- Israeli Academic Calculator/view/UserControls/DataEntryBox.xaml.cs
+++ Israeli Academic Calculator/view/UserControls/DataEntryBox.xaml.cs
@@ -24,5 +24,36 @@ namespace Israeli_Academic_Calculator.view.UserControls
         {
             InitializeComponent();
         }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set 
+            { 
+                placeholder = value; 
+                tbPlaceholder.Text = placeholder;
+            }
+        }
+
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtInput.Text))
+            {
+                tbPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                tbPlaceholder.Visibility = Visibility.Hidden;
+            }
+        }
     }
 }
